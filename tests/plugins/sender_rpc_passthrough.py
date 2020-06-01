70f43d4ceb685882c71b69e3786f4db8cc57e6d3
Noah Zamzow-Schmidt <nzamzow@mit.edu>
2020-06-01 15:18:06 -0500

    started tests

--- tests/plugins/sender_rpc_passthrough.py
+++ tests/plugins/sender_rpc_passthrough.py
@@ -2,16 +2,17 @@ THRESHOLD = 1000 #Msats below which we exclude channels from re-consideration
 def try_payment_on_path(plugin, best_route_index, amount, destination, payment_hash):
     #by here best_route is the route to send the unit on
     #update the value in the route info
-    route_tuple = plugin.routes_in_use[destination][best_route_index]
-    route_tuple[2] += amount
+    route_info= plugin.routes_in_use[destination][best_route_index]
+    route_info["amount_on_route"] += amount
 
     #now we actually pay the one unit on that route
     plugin.payment_hash_to_route[payment_hash] = best_route_index
-    plugin.rpc.sendpay(route_tuple[0], payment_hash)
+    plugin.log("attempting to send payment", payment_hash, "on route ", route_info["route"])
+    plugin.rpc.sendpay(route_info["route"], payment_hash)
 
 def send_more_transactions(plugin, destination, route_index):
-    route = plugin.routes_in_use[destination][route_index]
-    slack = route[1] - route[2]
+    route_info = plugin.routes_in_use[destination][route_index]
+    slack = route_info["window"] - route_info["amount_on_route"]
     while len(plugin.queue[destination]) > 0:
         oldest_payment = plugin.queue[destination][0]
         amount = None #TODO
@@ -23,40 +24,45 @@ def send_more_transactions(plugin, destination, route_index):
 
 @plugin.subscribe("sendpay_success")
 def handle_sendpay_success(plugin, sendpay_success):
-    plugin.log("receive a sendpay_success recored, id: {}, payment_hash: {}".format(sendpay_success['id'], sendpay_success['payment_hash']))
-
+    plugin.log("receive a sendpay_success recored, id: {},
+                payment_hash: {}".format(sendpay_success['id'],
+                sendpay_success['payment_hash'])
+            )
     destination = sendpay_success['destination']
     amount = sendpay_success['msatoshi']
     payment_hash = sendpay_success['payment_hash']
     route_index = plugin.payment_hash_to_route[payment_hash]
     route_tuple = plugin.routes_in_use[destination][route_index]
-    route_tuple[2] -= amount
+    route_tuple["amount_on_route"] -= amount
 
     #update window
     summation = 0
     for routes in plugin.routes_in_use[destination]:
-        summation += routes[1]
-    route_tuple[1] += (plugin.alpha/summation)
-    slack = route_tuple[1] - route_tuple[2]
+        summation += routes["window"]
+    plugin.log("adding ", plugin.alpha/summation, " to window")
+    route_tuple["window"] += (plugin.alpha/summation)
+    slack = route_tuple["window"] - route_tuple["amount_on_route"]
     del plugin.payment_hash_to_route[payment_hash]
+
     send_more_transactions(plugin, destination, route_index)
 
 
 @plugin.subscribe("sendpay_failure")
 def handle_sendpay_failure(plugin, sendpay_failure):
-    plugin.log("receive a sendpay_failure recored, id: {}, \
-            payment_hash: {}".format(sendpay_failure['data']['id'],\
-            sendpay_failure['data']['payment_hash']))
+    plugin.log("receive a sendpay_failure recored, id: {},
+     payment_hash: {}".format(sendpay_failure['data']['id'],
+               sendpay_failure['data']['payment_hash']))
 
     destination = sendpay_failure['data']['destination']
     amount = sendpay_failure['data']['msatoshi']
     payment_hash = sendpay_failure['data']['payment_hash']
     route_index = plugin.payment_hash_to_route[payment_hash]
     route_tuple = plugin.routes_in_use[destination][route_index]
-    route_tuple[2] -= amount
+    route_tuple["amount_on_route"] -= amount
 
     #update window
-    route_tuple[1] = max(route_tuple[2] - plugin.beta, 1)
+    plugin.log("removing ", plugin.beta, " from window")
+    route_tuple["window"] = max(route_tuple["amount_on_route"] - plugin.beta, 1)
     delete plugin.payment_hash_to_route[payment_hash]
 
 
@@ -69,14 +75,19 @@ def spider_pay(plugin, invoice):
 
     if destination not in plugin.routes_in_use:
         plugin.routes_in_use[destination] = []
-        #routes = find all possible edge disjoint widest paths (maximum minimum weight on a path) (choose 4)
+        #routes = find all possible edge disjoint widest paths
+        #(maximum minimum weight on a path) (choose 4)
         excludes = []
         window = 1
 
         for i in range(4):
             r = plugin.rpc.getroute(destination, plugin.payment_size, riskfactor=1,
                                     cltv=9, exclude=excludes)
-            plugin.routes_in_use[destination].append((r, window, 0))
+            route_info = {"route": r,
+                          "window": window,
+                          "amount_on_route": 0
+            }
+            plugin.routes_in_use[destination].append(route_info)
 
             for c in route:
                 if c['channel']['amount_msat'] < THRESHOLD:
@@ -92,10 +103,10 @@ def spider_pay(plugin, invoice):
     best_route_index = None
     #find biggest gap between window and what is being sent
     for i, route_info in enumerate(random_shuffle(plugin.routes_in_use[destination])):
-        slack = route_info[1] - route_info[2]
+        slack = route_info["window"] - route_info["amount_on_route"]
         if slack >= amount:
             best_route_slack = slack
-            best_route = route_info[0]
+            best_route = route_info["route"]
             best_route_index = i
             break
 
---
+++ tests/plugins/test_spider_pay.py
@@ -0,0 +1,66 @@
+# A lot of this code has been helped by cdecker
+
+from pyln.testing.fixtures import *
+from time import time
+
+plugin_path = os.path.join(os.path.dirname(__file__), "sender_rpc_passthrough.py")
+
+""" make sure regular payment with channel balances goes through
+"""
+def test_regressive(node_factory, executor):
+    """Line graph with the middle node (l2) running the plugin.
+    """
+    l1, l2, l3 = node_factory.line_graph(
+        3,  # We want 3 nodes
+        opts=[{}, {'plugin': plugin_path}, {}],  # Start l2 with plugin
+        wait_for_announce=True  # Let nodes finish gossip before returning
+    )
+
+    inv = l3.rpc.invoice(42, "lbl{:}".format(int(time())), "description")['bolt11']
+
+    # Let the pay run in the background (on an executor thread) so we don't
+    # wait for the pay to succeed before we can check in with the plugin.
+    f = executor.submit(l1.rpc.pay, inv)
+
+    # Now see that the plugin queues it
+    l2.daemon.wait_for_log(r'Queueing HTLC')
+
+    # Now retrieve the result from the `pay` task we passed to the executor
+    # above. If it failed the exception would get re-raised and fail this
+    # test, so just retrieving is enough to check it went through
+    f.result()
+
+
+"""  when there isn't insufficient balance, payment gets queued and completed later
+"""
+def test_payment_completion(node_factory, executor):
+    l1, l2, l3 = node_factory.line_graph(
+        3,  # We want 3 nodes
+        opts=[{}, {'plugin': plugin_path}, {}],  # Start l2 with plugin
+        wait_for_announce=True  # Let nodes finish gossip before returning
+    )
+
+    # TODO: need way of setting balances on payment channels
+    completed = []
+    futures = []
+    def trypay(i, sender, receiver):
+        # Small helper initiating a payment and then inserting itself into the
+        # list of completed order in the order of completion.
+        inv = receiver.rpc.invoice(42, "lbl{:}".format(i), "description")['bolt11']
+        sender.rpc.pay(inv)
+        completed.append(i)
+
+    # Queue all payment attempts, and remember the futures.
+    for i in range(10):
+        sender = l1 if i % 2 == 0 else l3
+        receiver = l3 if i % 2 == 0 else l1
+        futures.append(executor.submit(trypay, i, sender, receiver))
+
+    # Now wait for all futures to complete.
+    for f in futures:
+        f.result()
+
+    # Now check that all of them completed, should be FIFO
+    assert(len(completed) == 10)
+    print(completed)
+    #assert(completed == list(range(10)))
