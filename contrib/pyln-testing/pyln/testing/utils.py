98923c7f0d7232357e2547d0d73d22637a371741
Jan Sarenik <jajomojo@gmail.com>
2020-09-24 13:36:39 +0200

    pyln: utils:BitcoinD: add -wallet="test" option
    Per https://github.com/bitcoin/bitcoin/pull/15454

--- contrib/pyln-testing/pyln/testing/utils.py
+++ contrib/pyln-testing/pyln/testing/utils.py
@@ -349,8 +349,8 @@ class BitcoinD(TailableProc):
             '-logtimestamps',
             '-nolisten',
             '-txindex',
-            '-addresstype=bech32',
-	    '-fallbackfee=0.00001'
+            '-wallet="test"',
+            '-addresstype=bech32'
         ]
         # For up to and including 0.16.1, this needs to be in main section.
         BITCOIND_CONFIG['rpcport'] = rpcport
